118d5e32d4251e8986af9e81ccafddc6847689ee
moco <292237123@qq.com>
2015-07-15 13:28:27 +0800

    Update EJ09.js

--- E01/js/EJ09.js
+++ E01/js/EJ09.js
@@ -5,3 +5,5 @@
 //9 attachEvent addEventListener  第三个参数的作用 说明 有什么作用？
 
 //addEventListener() 第三个参数是boolean，当它为true时，函数将注册为捕获事件处理程序，并在事件不同的调度阶段调用。
+
+//false代表支持浏览器事件冒泡功能，true代表支持浏览事件捕获功能．最好理解一下什么叫事件模型
